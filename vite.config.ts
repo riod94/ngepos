f66573acff7125935bc3877a145ab9590ccd9cdc
Rio D <riyo.s94@gmail.com>
2026-04-19 18:01:29 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,24 @@ import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const port = parseInt(process.env.PORT || env.PORT || "3000");
 
   return {
+    server: {
+      port,
+      strictPort: true,
+      host: true,
+    },
+    preview: {
+      port,
+      strictPort: true,
+      host: true,
+    },
     plugins: [
       solidStart({
         ssr: false
       }),
       nitroV2Plugin(),
     ],
-    define: {
-      "process.env.DATABASE_URL": JSON.stringify(env.DATABASE_URL),
-      "process.env.JWT_SECRET": JSON.stringify(env.JWT_SECRET),
-      "process.env.SITE_URL": JSON.stringify(env.SITE_URL),
-      "process.env.SMTP_HOST": JSON.stringify(env.SMTP_HOST),
-      "process.env.SMTP_PORT": JSON.stringify(env.SMTP_PORT),
-      "process.env.SMTP_USER": JSON.stringify(env.SMTP_USER),
-      "process.env.SMTP_PASS": JSON.stringify(env.SMTP_PASS),
-      "process.env.SMTP_FROM": JSON.stringify(env.SMTP_FROM),
-    },
   };
 });
